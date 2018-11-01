0364c90ed92676c3ddcb4160c7648b4c6a41396e
linqiaozhou <873008568@qq.com>
2018-11-01 22:55:07 +0800

    sgd

--- Day4_SGD.py
+++ Day4_SGD.py
@@ -13,28 +13,37 @@ class SGD:
     def get_xy(self):
         return self.x,self.y
     def partial_reci_x(self):
-        self.deta_x= -2*self.x-200*(self.y-self.x*self.x)*(-2*self.x)
+        self.deta_x= -2*(1-self.x)-200*(self.y-self.x*self.x)*(-2*self.x)
+        return self.deta_x
     def partial_reci_y(self):
         self.deta_y= 200*(self.y-self.x*self.x)
+        return  self.deta_y
     def update(self):
         self.x -= self.lr*self.deta_x
         self.y -= self.lr*self.deta_y 
         return
-    def obj_fun(x,y):
-        return pow(1-x,2)+100*pow((y-pow(x,2)),2)
+    def obj_fun(self):
+        return (1-self.x)**2+100*(self.y-self.x**2)**2
     def cal_loss(self):
-        return pow((0-obj_fun(self.x,self.y)),2)
+        #print self.obj_fun()
+        #return pow((0-self.obj_fun()),2)
+        return abs(self.obj_fun())
 if __name__ == '__main__':
     iter = 0
-    lr = 0.1
-    sp_sgd = SGD(0,0,0.1)
-    while(sp_sgd.cal_loss>1 or iter > 10000):
+    lr = 0.0001
+    sp_sgd = SGD(0,0,lr)
+    while(sp_sgd.cal_loss()>0.001 and iter < 10000):
         iter += 1
-        sp_sgd.partial_reci_x()
-        sp_sgd.partial_reci_y()
+        print iter
+        print sp_sgd.partial_reci_x()
+        print sp_sgd.partial_reci_y()
         sp_sgd.update()
-    print sp_sgd.loss()
+        print sp_sgd.get_xy()
+        print sp_sgd.cal_loss()
+        #if iter/100.0 == 0.0:
+            #print iter,sp_sgd.cal_loss()
+    print sp_sgd.cal_loss()
         
-    x,y = sp.sgd.get_xy()
+    x,y = sp_sgd.get_xy()
     print x,y
 
