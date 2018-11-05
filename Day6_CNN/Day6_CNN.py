92d84a9dc21c1b386ffb295280a3ad8a2b13cbf0
linqiaozhou <873008568@qq.com>
2018-11-05 20:11:49 +0800

    Update Day6_CNN.py

--- Day6_CNN/Day6_CNN.py
+++ Day6_CNN/Day6_CNN.py
@@ -2,26 +2,31 @@
 import numpy as np
 
 class ConvLayer(object):
-    def __init__(self,kernel,padding_size,stride):
+    def __init__(self,kernel,kernel_num,padding_size,stride):
         self.kernel = kernel
+        self.kernel_num=kernel_num
         self.padding_size = padding_size
         self.stride = stride
-    def conv2d(self,feature):
+    def conv2d(self,input_data):
         input_h,input_w = feature.shape
         k_h,k_w=self.kernel.shape
         input_h += 2*self.padding_size
         input_w += 2*self.padding_size
         output_h = input_h/self.stride
         output_w = input_w/self.stride
+        padding_input = padding(input_data)
         ret = np.zeros((output_w,output_h),dtype = np.float)
         for h in range(input_h-2*self.padding_size):
             for w in range(input_w-2*self.padding_size):
-                for kh in range(k_h):
-                    for kw in range(k_w):
-                        res[h,w] += feature[h+kh,w+kw]*kernel[kh,kw]
+                temp = padding_input[h:h+k_h,w+w_h]
+                ret[h,w]=np.sum(temp*kernel)
         return ret
-  def padding(self,feature):
-      org_h,org_w = feature.shape
-      np.zeros((org_h+2*self.padding_size,org_w+self.padding_size),dtype=np.float)
-      
+  def padding(self,input_data):
+      org_h,org_w = input_data.shape
+      ret = np.zeros((org_h+2*self.padding_size,org_w+self.padding_size),dtype=np.float)
+      ret[self.padding_size:org_h+self.padding_size,self.padding_size:org_w+self.padding_size] = input_data
+      return ret
+  def forward(self,batch_size,input_data):
+        
+        
       
