1d9ab830fd62641d633ab4e440d7857cb783194d
linqiaozhou <873008568@qq.com>
2018-11-14 19:36:45 +0800

    Update Day6_bn.py

--- Day6_CNN/Day6_bn.py
+++ Day6_CNN/Day6_bn.py
@@ -2,15 +2,17 @@
 import numpy as np
 import random
 class BatchNormal(object):
-    def __init__(self,channel):
+    def __init__(self,input_data):
+        batch_size,channel,img_h,img_w=input_data.shape
         self.channel = channel
-        self.norm = np.zeros(channel,dtype=float32)
+        self.norm = np.zeros_like(input_data,dtype=float32)
         self.mean = np.zeros(channel,dtype=float32)
         self.variance = np.zeros(channel,dtype=float32)
         self.gammma = np.random.randn(channel,dtype=float32)
         self.beta=np.random.randn(channel,dtype=float32)
     def forward(self,input_data):
         batch_size,channel,img_h,img_w=input_data.shape
+        output_data = np.zeros_like(input_data,dtype=float32)
         for b in range(batch_size):
             for c in range(channel):
                 for h in range(img_h):
@@ -28,6 +30,8 @@ class BatchNormal(object):
                 for h in range(img_h):
                     for w in range(img_w):
                         self.norm[b,c,h,w]= (input_data[b,c,h,w]-self.mean[c])/(self.variance[c]+0.0000001)
-                        
+                        output_data[b,c,h,w]= self.norm[b,c,h,w]*self.gamma[c]+self.beta[c]
+        return output_data
         
-        
+        def backward(self,residual):
+            
