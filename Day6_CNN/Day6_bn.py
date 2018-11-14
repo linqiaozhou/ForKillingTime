5cd26619ec5afa02ff4b1fa482bf9cfb123a2f20
linqiaozhou <873008568@qq.com>
2018-11-14 18:44:42 +0800

    Update Day6_bn.py

--- Day6_CNN/Day6_bn.py
+++ Day6_CNN/Day6_bn.py
@@ -1,5 +1,16 @@
 #coding=uft-8
 import numpy as np
+import random
 class BatchNormal(object):
-    def __init__(self):
-        self.norm =     
+    def __init__(self,channel):
+        self.channel = channel
+        self.norm = np.zeros(channel,dtype=float32)
+        self.mean = np.zeros(channel,dtype=float32)
+        self.variance = np.zeros(channel,dtype=float32)
+        self.gammma = np.zeros(channel,dtype=float32)
+        self.beta=np.zeros(channel,dtype=float32)
+    def forward(self,input_data):
+        bath_size,channel,img_h,img_w=input_data.shape
+        self.mean = np.mean(input_data,axis=2)
+        self.varivance=np.varivance(input_data,axis=2)
+        
