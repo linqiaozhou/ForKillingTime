a1b9a7f10b84aaac67a31c49aafd3658042b63bd
linqiaozhou <873008568@qq.com>
2018-11-14 19:19:25 +0800

    Update Day6_bn.py

--- Day6_CNN/Day6_bn.py
+++ Day6_CNN/Day6_bn.py
@@ -7,10 +7,27 @@ class BatchNormal(object):
         self.norm = np.zeros(channel,dtype=float32)
         self.mean = np.zeros(channel,dtype=float32)
         self.variance = np.zeros(channel,dtype=float32)
-        self.gammma = np.zeros(channel,dtype=float32)
-        self.beta=np.zeros(channel,dtype=float32)
+        self.gammma = np.random.randn(channel,dtype=float32)
+        self.beta=np.random.randn(channel,dtype=float32)
     def forward(self,input_data):
-        bath_size,channel,img_h,img_w=input_data.shape
-        self.mean = np.mean(input_data,axis=2)
-        self.varivance=np.varivance(input_data,axis=2)
+        batch_size,channel,img_h,img_w=input_data.shape
+        for b in range(batch_size):
+            for c in range(channel):
+                for h in range(img_h):
+                    for w in range(img_w):
+                        self.mean[c] += input_data[b,c,h,w]
+        self.mean/=input_data.size
+        for b in range(batch_size):
+            for c in range(channel):
+                for h in range(img_h):
+                    for w in range(img_w):
+                        self.variance[c] += (input_data[b,c,h,w]-self.mean[c])**2
+        self.variance /= input_data.size
+        for b in range(batch_size):
+            for c in range(channel):
+                for h in range(img_h):
+                    for w in range(img_w):
+                        self.norm[b,c,h,w]= (input_data[b,c,h,w]-self.mean[c])/(self.variance[c]+0.0000001)
+                        
+        
         
