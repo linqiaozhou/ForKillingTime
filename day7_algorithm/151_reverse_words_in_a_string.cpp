d2829c2bbe00514a82dcfe50d61aff5611d063b9
linqiaozhou <873008568@qq.com>
2019-01-25 17:30:53 +0800

    Update 151_reverse_words_in_a_string.cpp

--- day7_algorithm/151_reverse_words_in_a_string.cpp
+++ day7_algorithm/151_reverse_words_in_a_string.cpp
@@ -11,7 +11,32 @@ Follow up: For C programmers, try to solve it in-place in O(1) space.
 
 class Solution {
 public:
+    void swap(int& a,int& b)
+    {
+        int temp = a;
+        a=b;
+        b=temp;
+    }
+    void reverse
     void reverseWords(string &s) {
-        
+        int start = 0;
+        int end = s.size();
+        while(start<end)
+        {
+            swap(s[start],s[end]);
+            start++;
+            end--;
+        }
+        //先考虑没有异常空格情况
+        start = 0;
+        end = start;
+        while(start!='\0')
+        {
+            while(s[end]!=' '&&s[end]!='\0')
+            {
+                end++;
+            }
+            
+        }
     }
 };
