c5ec4dbf92331b6d75c92e15ee8a28f5d77f5dae
linqiaozhou <873008568@qq.com>
2019-01-27 21:08:59 +0800

    Update 151_reverse_words_in_a_string.cpp

--- day7_algorithm/151_reverse_words_in_a_string.cpp
+++ day7_algorithm/151_reverse_words_in_a_string.cpp
@@ -11,32 +11,98 @@ Follow up: For C programmers, try to solve it in-place in O(1) space.
 
 class Solution {
 public:
-    void swap(int& a,int& b)
+    void swap(char& a,char& b)
     {
-        int temp = a;
+        char temp = a;
         a=b;
         b=temp;
     }
-    void reverse
-    void reverseWords(string &s) {
-        int start = 0;
-        int end = s.size();
+    void reverseStr(string&s, int start, int end)
+    {
         while(start<end)
         {
             swap(s[start],s[end]);
             start++;
             end--;
         }
-        //先考虑没有异常空格情况
+    }
+    void reverseWords(string &s) {
+    	int len = s.size();
+        int start = 0;
+        int end = len - 1;
+        
+        reverseStr(s, start, end);
+
+        start = 0;
+        end = start;   
+        //process leading spaces
+        while(end != len && s[end] == ' ')
+        {
+    	    end++;
+        }
+        int space_num = end - start;
+        if(space_num > 0)
+        {
+        	len -= space_num; 
+        	for(int i = 0; i < len; i++)
+        	{
+        		s[start+i] = s[end + i];
+        	}
+        }
+
+        //while(s[start]!='\0')
+        while(start != len)
+        {	
+        	while(start != len && s[start] != ' ')
+        	{
+        		start++;
+        	}
+        	if(start == len) break;
+            end = start;  
+            while(end != len && s[end] == ' ')
+        	{
+        		end++;
+        	}
+        	space_num = end - start;
+        	//process trailing spaces
+        	if(end == len && space_num > 0)
+			{
+				for(int i = 0; i < space_num; i++)
+				{
+					s[start+i] = s[end+i];
+				}
+				len -= space_num; 
+				break;
+			}
+			else if(space_num > 1)
+			{
+			    len = len - space_num + 1;
+				
+				for(int i = 0; i < len; i++)
+				{
+					s[start + 1 +i] = s[end+i];
+				}
+			}
+			start++;
+        }
+        s.resize(len);
+        //reverse the word
         start = 0;
         end = start;
-        while(start!='\0')
+        while(start != len)
         {
-            while(s[end]!=' '&&s[end]!='\0')
+            while(end != len && s[end] != ' ')
             {
                 end++;
             }
-            
+            if(end == len)
+            {
+            	reverseStr(s,start,end-1);
+            	break;
+            }
+            reverseStr(s,start,end-1);
+            end++;
+            start = end;
         }
     }
 };
