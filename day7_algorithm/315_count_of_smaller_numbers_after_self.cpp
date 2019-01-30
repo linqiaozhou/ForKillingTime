fe5d86cceefa0296d1451a9d4acf5e37d51a8f7f
linqiaozhou <873008568@qq.com>
2019-01-30 14:53:27 +0800

    Update 315_count_of_smaller_numbers_after_self.cpp

--- day7_algorithm/315_count_of_smaller_numbers_after_self.cpp
+++ day7_algorithm/315_count_of_smaller_numbers_after_self.cpp
@@ -1,7 +1,6 @@
-/*You are given an integer array nums and you have to return a new counts array. The counts array has the property where counts[i] is the number of smaller elements to the right of nums[i].
-
+/*You are given an integer array nums and you have to return a new counts array. 
+The counts array has the property where counts[i] is the number of smaller elements to the right of nums[i].
 Example:
-
 Input: [5,2,6,1]
 Output: [2,1,1,0] 
 Explanation:
@@ -13,6 +12,22 @@ To the right of 1 there is 0 smaller element.*/
 class Solution {
 public:
     vector<int> countSmaller(vector<int>& nums) {
+        int len = nums.size();
+        vector<int> ret; 
+        if(len == 0) return ret;
+        for(int i = 0; i < len; i++)
+        {
+            int cnt = 0;
+            for(int j = i + 1; j < len; j++)
+            {
+                if(nums[j] < nums[i])
+                {
+                    cnt++;
+                }
+            }
+            ret.push_back(cnt);
+        }
         
+        return ret;
     }
 };
